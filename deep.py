ad926d7eaa6ad916497de5a6e12f0ea70e95e76e
Giorgi Pkhakadze <pxakuna13@gmail.com>
2024-06-01 13:57:22 +0400

    Add Calc

---
+++ deep.py
@@ -0,0 +1,15 @@
+x = int(input("Put Number 1 : " ))
+y = str(input("(+,-,*,/) : "))
+z = int(input("Put Number 2 :"))
+
+if y == "+":
+    print(x + y)
+
+elif y == "-":
+    print(x - z)
+
+elif y == "*":
+    print(x * z)
+
+elif y == "/":
+    print(x / z)
